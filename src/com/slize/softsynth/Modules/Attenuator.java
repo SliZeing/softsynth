05253eebdb65e1a0ff6bf8ff191956440f677d7b
SliZeing <slize@slize.net>
2014-08-21 23:10:18 +0200

    Changed cv input handeling to be done in getSamples()

--- src/com/slize/softsynth/Modules/Attenuator.java
+++ src/com/slize/softsynth/Modules/Attenuator.java
@@ -7,51 +7,53 @@ public class Attenuator implements SampleProvider {
     private SampleProvider provider;
 
     private double attenuationRatio;
-    private SampleProvider cv;
+    private double attenuationBaseRatio;
+    private SampleProvider attenuationCv;
 
     public Attenuator() {
-        attenuationRatio = 1.0;
+        setAttenuationBaseRatio(1.0);
     }
 
-    private short attenuate(short sample, short cvSample) {
-        if(cv != null) {
-            double modulator = SampleConverter.getSampleValue(cvSample);
-
-            // The modulator should not be < 0.0, so we convert the value to a positive number if it's < 0.0.
-            if(modulator < 0.0) {
-                modulator *= -1;
-            }
-
-            // Apply modulation value to sample
-            if(attenuationRatio * modulator <= 1.0) {
-                sample *= attenuationRatio * modulator;
-            }
-        }
-        else {
-            sample *= attenuationRatio;
-        }
-
-        return sample;
+    private short attenuate(short sample) {
+        return (short) (sample * attenuationRatio);
     }
 
     @Override
     public int getSamples(byte[] buffer) {
-        byte[] cvBuffer = new byte[buffer.length];
+        byte[] cvBuffer = null;
 
         provider.getSamples(buffer);
 
-        if(cv != null) {
-            cv.getSamples(cvBuffer);
+        if(attenuationCv != null) {
+            cvBuffer = new byte[buffer.length];
+            attenuationCv.getSamples(cvBuffer);
         }
 
         int index = 0;
 
         for(int i = 0; i < buffer.length / 2; i++) {
+            // If there is cv input then apply that to the attenuationRatio
+            if(cvBuffer != null) {
+                short cvSample = SampleConverter.toSample(cvBuffer, index);
+                double cvValue = SampleConverter.getSampleValue(cvSample);
+
+
+                if(cvValue < 0.0) {
+                    cvValue *= -1;
+                }
+
+                // Apply modulation value to sample
+                if(attenuationRatio * cvValue <= 1.0) {
+                    attenuationRatio = attenuationBaseRatio * cvValue;
+                }
+                else {
+                    attenuationRatio = 1.0;
+                }
+            }
+
             short sample = SampleConverter.toSample(buffer, index);
 
-            short cvSample = SampleConverter.toSample(cvBuffer, index);
-
-            sample = attenuate(sample, cvSample);
+            sample = attenuate(sample);
 
             // Store the processed sample back to the buffer.
             buffer[index++] = (byte)(sample >> 8);
@@ -65,16 +67,16 @@ public class Attenuator implements SampleProvider {
         this.provider = provider;
     }
 
-    public void setAttenuationRatio(double attenuationRatio) {
+    public void setAttenuationBaseRatio(double attenuationRatio) {
         if(attenuationRatio >= 0.0 && attenuationRatio <= 1.0) {
-            this.attenuationRatio = attenuationRatio;
+            this.attenuationBaseRatio = attenuationRatio;
         }
         else {
             throw new IllegalArgumentException("attenuationRatio has to be between 0.0 and 1.0");
         }
     }
 
-    public void setCv(SampleProvider provider) {
-        this.cv = provider;
+    public void setAttenuationCv(SampleProvider provider) {
+        this.attenuationCv = provider;
     }
 }
