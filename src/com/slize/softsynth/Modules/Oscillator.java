21d4d14c7e7e3827cd41a8aa5ed2beca4584dbe2
SliZeing <slize@slize.net>
2014-08-22 23:09:32 +0200

    Added modulation for the Pulse Width via CV on the Oscillator

--- src/com/slize/softsynth/Modules/Oscillator.java
+++ src/com/slize/softsynth/Modules/Oscillator.java
@@ -1,6 +1,7 @@
 package com.slize.softsynth.Modules;
 
 import com.slize.softsynth.Engien.Output;
+import com.slize.softsynth.Engien.SampleConverter;
 import com.slize.softsynth.Engien.SampleProvider;
 import com.slize.softsynth.utlis.Waveform;
 
@@ -8,13 +9,16 @@ public class Oscillator implements SampleProvider {
     private Waveform waveform;
     private long samplesPerPeriod;
     private long sampleNumber;
+
     private double pulseWidth;
+    private double pulseWidthBaseValue;
+    private SampleProvider pulseWidthCvProvider;
 
 
     public Oscillator() {
         waveform = Waveform.SIN;
         setFrequency(440.0);
-        setPulseWidth(0.5);
+        setPulseWidthBaseValue(0.5);
     }
 
     private double getSample() {
@@ -49,8 +53,31 @@ public class Oscillator implements SampleProvider {
 
     @Override
     public int getSamples(byte[] buffer) {
+        byte[] pwmBuffer = null;
+
+        if(pulseWidthCvProvider != null) {
+            pwmBuffer = new byte[buffer.length];
+            pulseWidthCvProvider.getSamples(pwmBuffer);
+        }
+
         int index = 0;
         for (int i = 0; i < (buffer.length / 2); i++) {
+            if(pwmBuffer != null) {
+                short pwmSample = SampleConverter.toSample(pwmBuffer, index);
+                double pwmValue = SampleConverter.getSampleValue(pwmSample) * 2;
+
+                if(pwmValue < 0.0) {
+                    pwmValue *= -1;
+                }
+
+                if(pulseWidthBaseValue * pwmValue <= 1.0) {
+                    pulseWidth = pulseWidthBaseValue * pwmValue;
+                }
+                else {
+                    pulseWidth = 1.0;
+                }
+            }
+
             double ds = getSample() * Short.MAX_VALUE;
             short ss = (short) Math.round(ds);
             buffer[index++] = (byte)(ss >> 8);
@@ -64,13 +91,18 @@ public class Oscillator implements SampleProvider {
         this.samplesPerPeriod = (long)(Output.getSampleRate() / frequency);
     }
 
-    public void setPulseWidth(double pulseWidth) {
+    public void setPulseWidthBaseValue(double pulseWidth) {
         if(!(pulseWidth > 1.0 || pulseWidth < 0.0)) {
-            this.pulseWidth = pulseWidth;
+            this.pulseWidthBaseValue = pulseWidth;
+            this.pulseWidth = this.pulseWidthBaseValue;
         }
         else {
             throw new IllegalArgumentException("The pulse width has to be between 0.0 and 1.0");
         }
+    }
+
+    public void setPulseWidthCvProvider(SampleProvider provider) {
+        this.pulseWidthCvProvider = provider;
     }
 
     public void setWaveform(Waveform waveform) {
